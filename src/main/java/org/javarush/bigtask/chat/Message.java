006b911a6ef41b21b2767cb3acbebe22690c4d69
Anatoliy Ivanov <plx9421@gmail.com>
2016-04-30 12:01:08 +0300

    first init

---
+++ src/main/java/org/javarush/bigtask/chat/Message.java
@@ -0,0 +1,36 @@
+package org.javarush.bigtask.chat;
+
+import java.io.Serializable;
+
+/**
+ * Created by Alexey on 01.03.2016.
+ */
+public class Message implements Serializable
+{
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type)
+    {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data)
+    {
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType()
+    {
+        return type;
+    }
+
+    public String getData()
+    {
+        return data;
+    }
+
+
+}
---
+++ src/main/java/org/javarush/bigtask/chat/MessageType.java
@@ -0,0 +1,14 @@
+package org.javarush.bigtask.chat;
+
+/**
+ * Created by Alexey on 01.03.2016.
+ */
+public enum MessageType
+{
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED;
+}
